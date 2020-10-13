7d44220e57d965a51986a0075b3fac7b10e89e95
Grayson Yang <grayson.yang@quest.com>
2020-10-13 10:05:43 +0800

    provide API /2/timeline/media/<string:screen_name>.json

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify, abort, make_response, request
 from library.twitter_dl import TwitterDownloader
 import urllib.parse
 
+from library.twitter_list_dl import TwitterMediaViewer
+
 app = Flask(__name__)
 
 
@@ -38,20 +40,36 @@ def get_twitter_m3u8():
     debug = 0
     twitter_dl = TwitterDownloader(twitter_url, output_dir, resolution, debug)
     player_config = twitter_dl.get_playlist()
-    playbackUrl = player_config.get("track").get("playbackUrl")
-    print('playbackUrl = ' + playbackUrl)
+    if 'errors' in player_config is False:
+        playbackUrl = player_config.get("track").get("playbackUrl")
+        print('playbackUrl = ' + playbackUrl)
 
-    m3u8_url_parse = urllib.parse.urlparse(playbackUrl)
-    video_host = 'http' + '://' + '10.154.10.111:8081' + m3u8_url_parse.path
+        m3u8_url_parse = urllib.parse.urlparse(playbackUrl)
+        video_host = 'http' + '://' + '10.154.10.111:8081' + m3u8_url_parse.path
 
-    track = {
-        "contentType": "media_entity",
-        "durationMs": 140000,
-        "playbackUrl": video_host,
-        "playbackType": "application/x-mpegURL"
-    }
-    startDownload(twitter_url)
-    return jsonify({'track': track}), 200
+        track = {
+            "contentType": "media_entity",
+            "durationMs": 140000,
+            "playbackUrl": video_host,
+            "playbackType": "application/x-mpegURL"
+        }
+        startDownload(twitter_url)
+        return jsonify({'track': track}), 200
+    else:
+        return jsonify(player_config.get('errors')), 404
+
+
+@app.route('/2/timeline/media/<string:screen_name>.json', methods=['GET'])
+def get_media_tweets(screen_name):
+    twitter_home_url = 'https://twitter.com/'
+    screen_name_url = twitter_home_url + screen_name
+
+    media_viewer = TwitterMediaViewer(screen_name_url, 'D:/output')
+    tweets = media_viewer.get_tweets_from_disk()
+    if tweets is None:
+        return jsonify({'tweets': []})
+    video_list = media_viewer.filter_tweets_video(tweets)
+    return jsonify({'tweets': video_list})
 
 
 if __name__ == '__main__':
